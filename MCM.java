91b9b6e31e9f55aee9ad14a742745612935cfc93
harshpatel1995 <harshpatel@knights.ucf.edu>
2016-05-01 17:22:51 -0400

    Added files via upload

--- MCM.java
+++ MCM.java
@@ -1,89 +1,61 @@
-import java.io.*;
-import java.util.*;
-import java.lang.StringBuffer;
 
-public class MCM
-{
-	public int N[][];
-	public int d[];
-	public int SIZE = 5;
+public class MCM {
 
-	public static void main( String [ ] args ) throws Exception
-    {
-    	MCM mcm = new MCM();
-
-    	// Initialize N and d
-    	mcm.init();
-
-    	// Compute the minimum number of element multiplications required for the matrices defined in init.
-    	int result = mcm.minMCM();
-
-    	System.out.println("result = " + result);
+    public static void main(String[] args) {
+        MCM mcm = new MCM(5);
+        mcm.intialize();
+        System.out.println(mcm.minMCM());
     }
 
-	// This method initializes N and d
-	// The dimensions of d are hard-coded
-	public void init()
-	{
-		N = new int[SIZE][SIZE];
-    	d = new int[SIZE+1];
+    public int matrix[][];
+    public int dimensions[];
 
-		// N is initialized with everything at infinity, except the diagonals.
-		for (int i = 0; i < SIZE; i++)
-    	{
-    		for (int j = 0; j < SIZE; j++)
-    		{
-    			if (i == j)
-    				N[i][j] = 0;
-    			else
-    				N[i][j] = 999;
-    		}
-    	}
-
-		// d stores the dimensions of the arrays, as defined in the notes A0 is a d0xd1 matrix, A1 is a d1xd2 matrix, etc.
-		// which is why d has SIZE + 1 dimensions.
-    	d[0] = 2;
-		d[1] = 4;
-		d[2] = 2;
-		d[3] = 3;
-		d[4] = 1;
-		d[5] = 4;
-	}
-
-	// This method calculates the minimum number of element multiplications required
-	// to multiply the number of matrices given and their given dimensions.
-    public int minMCM()
-    {
-    	// Algorithm defined in Arup's CS II notes.
-    	// Basically we are looking for the optimal way to "split" the matrices, where k defines the "split".
-    	for (int i = 1; i <= SIZE; i++)
-    	{
-    		for (int j = 0; j <= SIZE-i; j++)
-    		{
-    			for (int k = j; k < j+i-1; k++)
-    			{
-    				if (N[j][j+i-1] > N[j][k]+N[k+1][j+i-1] + d[j]*d[k+1]*d[i+j])
-    				{
-    					N[j][j+i-1] = N[j][k]+N[k+1][j+i-1] + d[j]*d[k+1]*d[i+j];
-    				}
-    			}
-    		}
-    	}
-
-		// Output the final N matrix
-    	System.out.println("N = ");
-    	for (int i = 0; i < SIZE; i++)
-    	{
-    		for (int j = 0; j < SIZE; j++)
-    		{
-    			System.out.print(N[i][j] + "\t");
-    		}
-    		System.out.println();
-    	}
-
-		System.out.println();
-
-		// The result is stored in the first row, last column of N
-		return N[0][SIZE-1];
+    public MCM (int numMatrix) {
+        this.matrix = new int[numMatrix][numMatrix];
+        this.dimensions = new int[numMatrix+1];
     }
+
+    //Intialize the matrix here
+    public void intialize() {
+
+        for (int i = 0; i < this.matrix.length; i++) {
+            for (int j = 0; j < this.matrix.length; j++) {
+
+                if (i == j) this.matrix[i][j] = 0;
+                else this.matrix[i][j] = Integer.MAX_VALUE;
+
+            }
+        }
+
+        this.dimensions[0] = 2;
+        this.dimensions[1] = 4;
+        this.dimensions[2] = 2;
+        this.dimensions[3] = 3;
+        this.dimensions[4] = 1;
+        this.dimensions[5] = 4;
+
+    }
+
+    public int minMCM() {
+        for(int i = 1; i <= matrix.length; i++) {
+
+            for(int j = 0; j <= matrix.length - i; j++) {
+
+                for(int k = j; k < j+i-1; k++) {
+
+                    int current = matrix[j][j+i-1];
+                    int alternative = matrix[j][k]+matrix[k+1][j+i-1] + dimensions[j]*dimensions[k+1]*dimensions[i+j];
+
+                    if(current > alternative) {
+                        matrix[j][j+i-1] = alternative;
+                    }
+
+                }
+            }
+        }
+        //The least cost of MCM
+        return matrix[0][matrix.length - 1];
+    }
+
+
 }
